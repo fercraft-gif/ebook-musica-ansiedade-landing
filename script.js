28baf7a4036262f46f5dfb3cece8c47083657737
Fernanda Franzoni Zaguini <fercraft@gmail.com>
2025-11-19 13:54:02 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    alert('O download do ebook será iniciado em breve!');
+    // Aqui você pode adicionar a lógica para iniciar o download do arquivo
+});
